2088cd26c067c2f8f077f50e8d57b87ed194a1bd
gravo-lan <alvintsolh@gmail.com>
2023-09-07 14:53:17 +0800

    Fix negative rounding

--- src/Main.java
+++ src/Main.java
@@ -5,43 +5,43 @@ public class Main {
     }
 
     // 1. add
-    public int add(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
     }
     
     // 2. add
-    public int add(int a, int b, int c, int d) {
+    public static int add(int a, int b, int c, int d) {
         return a + b + c + d;
     }
 
     // 3. morningGreeting
-    public String morningGreeting(String name) {
+    public static String morningGreeting(String name) {
         return "早上好, " + name + "!";
     }
 
     // 4. afternoonGreeting
-    public String afternoonGreeting(String name) {
+    public static String afternoonGreeting(String name) {
         return "下午好, " + name + "!";
     }
 
     // 5. triple
-    public String triple(String name) {
+    public static String triple(String name) {
         return name.repeat(3);
     }
 
     // 6. half
-    public double half(double value) {
+    public static double half(double value) {
         return value / 2;
     }
 
     // 7. roundPositiveValueToNearestInteger
-    public int roundPositiveValueToNearestInteger(double value) {
+    public static int roundPositiveValueToNearestInteger(double value) {
         return (int) Math.round(value);
     }
 
     // 8. roundNegativeValueToNearestInteger
-    public int roundNegativeValueToNearestInteger(double value) {
-        return (int) Math.round(value);
+    public static int roundNegativeValueToNearestInteger(double value) {
+        return (int) Math.round(Math.abs(value)) * -1;
     }
 
 }
